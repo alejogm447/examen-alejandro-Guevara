fc6873522fcab790b5f7670999d7e77e3ca5de49
alejogm447 <alejandro_mendoza@otlook.es>
2017-10-10 19:50:57 -0600

    examen_seminario_ajgm

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+var datospersonales = {
+  cuenta:"0801199409441",
+  nombre:"Alejandro Jose Guevara Mendoza",
+  correo:"alejandro_mendoza@outlook.es"
+};
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+router.get('/0801199409441', function(req, res, next) {
+  res.json(datospersonales);
+});
+
+module.exports= router;
